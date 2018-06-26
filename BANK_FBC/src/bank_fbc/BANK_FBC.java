a941b0c6ac7fd5f558f36d2a35529e6055d12859
TerrenceTakunda <ttmunyunguma@gmail.com>
2018-06-26 03:47:51 +0200

    Some modifications, no big changes

---
+++ BANK_FBC/src/bank_fbc/BANK_FBC.java
@@ -0,0 +1,78 @@
+/*
+ * Terrence Takunda Munyunguma [https://github.com/TerrenceTakunda]
+ *  Copyright (C) 2018 ttmunyunguma@gmail.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package bank_fbc;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOPackager;
+import org.jpos.iso.ISORequestListener;
+import org.jpos.iso.ISOServer;
+import org.jpos.iso.ISOSource;
+import org.jpos.iso.ServerChannel;
+import org.jpos.iso.channel.ASCIIChannel;
+import org.jpos.iso.packager.ISO87APackager;
+import org.jpos.util.LogSource;
+import org.jpos.util.Logger;
+import org.jpos.util.SimpleLogListener;
+
+/**
+ *
+ * @author terrence
+ */
+public class BANK_FBC implements ISORequestListener{
+
+    public static void main(String[] args) throws ISOException {
+        
+        ISOPackager packager = new ISO87APackager();
+        Logger logger = new Logger();
+        logger.addListener(new SimpleLogListener(System.out));
+        ServerChannel sc = new ASCIIChannel("localhost", 7112, packager);
+
+        ((LogSource)sc).setLogger(logger, "fbc-bank-server-logger");
+        
+        ISOServer server = new ISOServer(7112, sc, null);
+        server.setLogger(logger, "server-logger");
+        server.addISORequestListener(new BANK_FBC());
+        
+        new Thread(server).start();
+        
+        ISOMsg m = new ISOMsg();
+        m.setPackager(packager);
+    }
+
+    @Override
+    public boolean process(ISOSource isos, ISOMsg isomsg) {
+        
+        ISOMsg m = (ISOMsg) isomsg.clone();
+        
+        try{
+            m.setResponseMTI();
+            m.set(39, "00");
+            
+            isos.send(m);
+        }
+        catch(IOException | ISOException ex){
+            java.util.logging.Logger.getLogger(BANK_FBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
+    }
+    
+}
---
+++ BANK_STANCHAT/src/bank_stanchat/BANK_STANCHAT.java
@@ -0,0 +1,63 @@
+/*
+ * Terrence Takunda Munyunguma [https://github.com/TerrenceTakunda]
+ *  Copyright (C) 2018 ttmunyunguma@gmail.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package bank_stanchat;
+
+/**
+ *
+ * @author terrence
+ */
+public class BANK_STANCHAT implements ISORequestListener{
+
+    public static void main(String[] args) throws ISOException {
+        
+        ISOPackager packager = new ISO87APackager();
+        Logger logger = new Logger();
+        logger.addListener(new SimpleLogListener(System.out));
+        ServerChannel sc = new ASCIIChannel("localhost", 7115, packager);
+
+        ((LogSource)sc).setLogger(logger, "stanbic-bank-server-logger");
+        
+        ISOServer server = new ISOServer(7115, sc, null);
+        server.setLogger(logger, "server-logger");
+        server.addISORequestListener(new BANK_STANCHAT());
+        
+        new Thread(server).start();
+        
+        ISOMsg m = new ISOMsg();
+        m.setPackager(packager);
+    }
+
+    @Override
+    public boolean process(ISOSource isos, ISOMsg isomsg) {
+        
+        ISOMsg m = (ISOMsg) isomsg.clone();
+        
+        try{
+            m.setResponseMTI();
+            m.set(39, "00");
+            
+            isos.send(m);
+        }
+        catch(IOException | ISOException ex){
+            java.util.logging.Logger.getLogger(BANK_STANCHAT.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
+    }
+    
+}
