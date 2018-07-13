838e21601554a7fdc55c3c1f88503b187fd0bd9b
TerrenceTakunda <ttmunyunguma@gmail.com>
2018-07-13 23:21:06 +0200

    Shoping Cart Update

---
+++ TelMarketShop/src/java/com/telmarket/dao/ListDao.java
@@ -0,0 +1,84 @@
+/*
+ * Terrence Takunda Munyunguma [https://github.com/TerrenceTakunda]
+ *  Copyright (C) 2018 ttmunyunguma@gmail.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package com.telmarket.dao;
+
+import com.telmarket.entity.Category;
+import com.telmarket.entity.Product;
+import com.telmarket.entity.SubCategory;
+import com.telmarket.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author terrence
+ */
+public class ListDao {
+    
+    public List catList(){
+        
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        List<Category> cList = session.createQuery("SELECT a1.catName FROM Category a1").list();
+        cList.toString();
+        session.close();
+        return cList;
+    }
+    
+    public List subcatList(String name){
+        
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        List<Category> scList = session.createQuery("SELECT a1.subCatName FROM SubCategory a1 WHERE a1.category.catId IN (SELECT a.catId FROM Category a WHERE lower(a.catName)= '"+name.toLowerCase()+"')").list();
+        scList.toString();
+        session.close();
+        return scList;
+    }
+    
+    public List<Category> catListByName(String name){
+        
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        List<Category> cList = session.createQuery("SELECT a1 FROM Category a1 WHERE lower(catName) = '"+name.toLowerCase()+"'").list();
+        cList.toString();
+        session.close();
+        return cList;
+    }
+    
+    public List<SubCategory> subcatListByName(String name){
+        
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        List<SubCategory> cList = session.createQuery("SELECT a1 FROM SubCategory a1 WHERE lower(subCatName) = '"+name.toLowerCase()+"'").list();
+        cList.toString();
+        session.close();
+        return cList;
+    }
+    
+    public List allProductsList(){
+        
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        List<Product> pList = session.createQuery("SELECT a1 FROM Product a1").list();
+        pList.toString();
+        session.close();
+        return pList;
+    }
+}
---
+++ TelMarketShop/src/java/com/telmarket/handler/DisplayProductMB.java
@@ -0,0 +1,68 @@
+/*
+ * Terrence Takunda Munyunguma [https://github.com/TerrenceTakunda]
+ *  Copyright (C) 2018 ttmunyunguma@gmail.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package com.telmarket.handler;
+
+import com.telmarket.dao.ListDao;
+import com.telmarket.entity.Product;
+import javax.inject.Named;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author terrence
+ */
+@ManagedBean
+@Named(value = "displayProductMB")
+@SessionScoped
+public class DisplayProductMB implements Serializable {
+
+    /**
+     * Creates a new instance of DisplayProductMB
+     */
+    Product product = new Product();
+    private Product selectedProduct;
+    
+    public DisplayProductMB() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Product getSelectedProduct() {
+        return selectedProduct;
+    }
+
+    public void setSelectedProduct(Product selectedProduct) {
+        this.selectedProduct = selectedProduct;
+    }
+    
+    public List<Product> getAllProducts(){
+        
+        List<Product> pList = new ListDao().allProductsList();
+        return pList;
+    }
+}
